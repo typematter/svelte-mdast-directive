d36491ec91a1804cd7841cf70bc84a1b9d94878e
Matthew Gibbons <mhgibbons@me.com>
2024-10-06 13:15:44 +0000

    Removed example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,2 @@
 <script lang="ts">
-	import { mdastDirectiveComponents } from '$lib/index.js';
-	import { Unist } from '@accuser/svelte-unist';
-	import type { PageData } from './$types.js';
-	import Highlight from './Highlight.svelte';
-
-	let { data }: { data: PageData } = $props();
-
-	let { ast } = $derived(data);
-
-	let components = { ...mdastDirectiveComponents, hl: Highlight };
 </script>
-
-<Unist {ast} {components} />

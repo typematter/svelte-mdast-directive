16c490788bc49552f130ccc45f020cc7e2e5a7b6
Matthew Gibbons <mhgibbons@me.com>
2024-10-06 06:29:17 +0000

    Extend `svelte-unist` module types

--- src/lib/defaults/mdast-directive-components.ts
+++ src/lib/defaults/mdast-directive-components.ts
@@ -1,13 +1,29 @@
 import ContainerDirective from '$lib/components/ContainerDirective.svelte';
 import LeafDirective from '$lib/components/LeafDirective.svelte';
 import TextDirective from '$lib/components/TextDirective.svelte';
-import type { Components, Nodes } from '@accuser/svelte-unist';
+import type { Components } from '@accuser/svelte-unist';
 
 declare module '@accuser/svelte-unist' {
-	export interface NodeMap {
+	export type AllDirectives = DirectiveMap[keyof DirectiveMap];
+
+	export interface ComponentMap {
 		containerDirective: import('mdast-util-directive').ContainerDirective;
 		leafDirective: import('mdast-util-directive').LeafDirective;
 		textDirective: import('mdast-util-directive').TextDirective;
+	}
+
+	export interface Context {
+		directives: Directives;
+	}
+
+	export interface DirectiveMap {}
+
+	export type Directives<T extends import('mdast-util-directive').Directives = AllDirectives> = {
+		[K in T['name']]: import('svelte').Component<Extract<T, { name: K }>> | undefined;
+	};
+
+	export interface Props {
+		directives?: Directives;
 	}
 }
 
@@ -15,4 +31,4 @@ export default {
 	containerDirective: ContainerDirective,
 	leafDirective: LeafDirective,
 	textDirective: TextDirective
-} satisfies Components<Nodes>;
+} satisfies Components;
